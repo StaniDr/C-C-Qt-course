6fde1aa455daae2a90d8b92e6b0935fdbe52c8fa
StaniDr <drobot_stanislav@mail.ru>
2019-10-26 16:15:14 +0300

    overload_char

---
+++ lesson_2/over_char.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(int argc, char **argv)
+{
+
+
+    char x = 127; 
+   printf("was x: %d \n", x);
+   printf("was x: %c \n", x);
+
+    x++; 
+   printf("was x: %d \n", x);
+   printf("was x: %c \n", x);
+
+    x++; 
+   printf("was x: %d \n", x);
+   printf("was x: %c \n", x);
+
+    return 0;
+
+
+}
